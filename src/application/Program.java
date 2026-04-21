46cd03cc288f0b0032537d48c95ac6008d577fc4
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-04-21 11:30:08 -0300

    Adicionei uma forma de acessar dados no banco de dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import db.DB;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -10,13 +11,14 @@ import java.util.Date;
 public class Program {
     public static void main(String[]args) {
 
-        System.out.println("O Java está procurando arquivos em: " + new java.io.File(".").getAbsolutePath());
+        //System.out.println("O Java está procurando arquivos em: " + new java.io.File(".").getAbsolutePath());
 
        SellerDao sellerDao = DaoFactory.createSellerDao();
 
        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
+
 
     }
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,6 +4,7 @@ import db.DbException;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -28,7 +29,7 @@ public class DB {
 		}
 		return conn;
 	}
-	
+
 	public static void closeConnection() {
 		if (conn != null) {
 			try {
@@ -38,18 +39,23 @@ public class DB {
 			}
 		}
 	}
-	
+
 	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")) {
+		try (InputStream is = new FileInputStream("db.properties")) {
+			if (is == null) {
+				throw new DbException("Arquivo db.properties não encontrado");
+
+			}
+
 			Properties props = new Properties();
-			props.load(fs);
+			props.load(is);
 			return props;
 		}
 		catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
-	
+
 	public static void closeStatement(Statement st) {
 		if (st != null) {
 			try {
