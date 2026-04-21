ac244a383535ed836e0237d33ce574c97deb88af
Lucas Fernando <lucasfernandosanchez298@gmail.com>
2026-04-21 15:25:25 -0300

    Adicionei o Delete em Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,9 +9,12 @@ import model.entities.Seller;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[]args) {
+
+        Scanner sc = new Scanner(System.in);
 
         //System.out.println("O Java está procurando arquivos em: " + new java.io.File(".").getAbsolutePath());
 
@@ -48,5 +51,12 @@ public class Program {
         sellerDao.update(seller);
         System.out.println("Update completed!");
 
+        System.out.println("\n ==== TEST 6: seller update ====");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed! ");
+
+        sc.close();
     }
 }
